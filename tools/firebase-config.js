f97960ad2638d1d12c405f6f048f59f8acb7fe08
millingtond <millingtond@mgs.org>
2025-12-24 08:43:10 +0000

    Update firebase-config.js

--- tools/firebase-config.js
+++ tools/firebase-config.js
@@ -1,10 +1,10 @@
 // Paste your Firebase Config object here
 // You get this from the Firebase Console -> Project Settings
 const FIREBASE_CONFIG = {
-apiKey: "AIzaSyBnXqzs7n7Q16Np-74z2lpYI44_M4wimtE",
-  authDomain: "roam-ec797.firebaseapp.com",
-  projectId: "roam-ec797",
-  storageBucket: "roam-ec797.firebasestorage.app",
-  messagingSenderId: "754058859162",
-  appId: "1:754058859162:web:9e9bf19b0caa1e4d32c523"
+apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
